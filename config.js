ea98a6da40338d8bfa3dc06e7aa95454f521d697
Smartabuot904 <smartquotex9045@gmail.com>
2026-05-13 02:18:21 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN:"8709620585:AAFdGdwRyZzH0r_KDR3hiWBI6Baqdr8Ho",
-  ADMIN_ID:8136997138,
-  CHANNEL:"@MiniDemoUpdate"
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  ADMIN_ID: 8136997138,
+  CHANNEL: "@MiniDemoUpdate"
 };
